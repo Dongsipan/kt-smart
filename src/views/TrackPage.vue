2eb144cf0562f9de0c17001516cd9043273cfd1f
dongsp <dongsp@yxt.com>
2023-06-15 00:13:12 +0800

    fix: 优化地图页面

--- src/views/TrackPage.vue
+++ src/views/TrackPage.vue
@@ -19,7 +19,7 @@
           <div class="dashboard-main">
             <div class="dashboard-main__data dashboard-main__data--outer">
               <div>altitude</div>
-              <div>--</div>
+              <div>{{ currentAltitude || "--" }}</div>
             </div>
             <div class="dashboard-main__action">
               <svg
@@ -36,23 +36,35 @@
                 ></path>
               </svg>
               <ion-button
-                v-show="!isRiding"
+                v-if="!isRiding"
                 class="dashboard-main__action-trigger"
                 shape="round"
                 @click="startRide"
-                >Start<br />
-                Ride
+              >
+                <span>Start<br />Ride</span>
               </ion-button>
-              <ion-button
-                v-show="isRiding"
-                ref="stopButtonRef"
-                v-on-long-press="onLongPressCallbackDirective"
-                class="dashboard-main__action-trigger"
-                color="secondary"
-                shape="round"
-                >Stop<br />
-                Ride
-              </ion-button>
+              <template v-else>
+                <ion-button
+                  v-if="!isStopRiding"
+                  @click="stopRide"
+                  v-on-long-press="onLongPressCallbackDirective"
+                  class="dashboard-main__action-trigger"
+                  color="success"
+                  shape="round"
+                >
+                  <span>Stop<br />Ride</span>
+                </ion-button>
+                <ion-button
+                  v-else
+                  @click="restoreRide"
+                  v-on-long-press="onLongPressCallbackDirective"
+                  class="dashboard-main__action-trigger"
+                  color="warning"
+                  shape="round"
+                >
+                  <span>Restore<br />Ride</span>
+                </ion-button>
+              </template>
               <div class="dashboard-main__data">
                 <div>mileage</div>
                 <div>--</div>
@@ -60,7 +72,7 @@
             </div>
             <div class="dashboard-main__data dashboard-main__data--outer">
               <div>speed</div>
-              <div>--</div>
+              <div>{{ currentSpeed || "--" }}</div>
             </div>
           </div>
           <div class="dashboard-info ion-margin-top">
@@ -78,6 +90,13 @@
             </div>
           </div>
         </div>
+        <ion-alert
+          :is-open="isOpenFinishAlert"
+          header="End cycling"
+          sub-header="Whether to end cycling"
+          :buttons="alertButtons"
+          @didDismiss="setOpenFinishAlert(false)"
+        ></ion-alert>
       </ion-toolbar>
     </ion-footer>
   </ion-page>
@@ -94,6 +113,7 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  IonAlert,
 } from "@ionic/vue";
 import { locateOutline } from "ionicons/icons";
 import AMapContainer from "@/components/AMapContainer.vue";
@@ -112,22 +132,26 @@ const setMapToCenter = () => {
 };
 const kalmanFilter = new GPSKalmanFilter();
 const currentTrack = ref<number[][]>([]);
-const currentSpeed = ref<number | null>(null);
-const currentAltitude = ref<number | null>(null);
+const currentSpeed = ref<number | string | null>(null);
+const currentAltitude = ref<number | string | null>(null);
 
 const isRiding = ref(false);
+const isStopRiding = ref(false);
 
 const startRide = () => {
   mapRef.value.initPolyline();
   isRiding.value = true;
+  watchPosition();
+};
+const watchPosition = () => {
   watchCurrentPosition(async (geolocationPosition) => {
     if (geolocationPosition === null) return;
     // await presentToast(JSON.stringify(geolocationPosition));
     console.log(geolocationPosition, geolocationPosition);
     const { longitude, latitude, altitude, speed, accuracy } =
       geolocationPosition!.coords;
-    currentSpeed.value = speed;
-    currentAltitude.value = altitude;
+    currentSpeed.value = speed ? ((speed * 3600) / 1000).toFixed(1) : speed;
+    currentAltitude.value = altitude ? altitude.toFixed(1) : altitude;
     const lnglat = kalmanFilter.filter(
       longitude,
       latitude,
@@ -143,17 +167,38 @@ const startRide = () => {
  * {"timestamp":1686647786114,"coords":{"altitude":32.022853851318359,"heading":-1,"latitude":31.298159747161719,"altitudeAccuracy":10.407867431640625,"longitude":120.54356498135141,"accuracy":46,"speed":-1}}
  * */
 const stopRide = () => {
-  isRiding.value = false;
+  isStopRiding.value = true;
   clearWatch();
 };
+const restoreRide = () => {
+  isStopRiding.value = false;
+  watchPosition();
+};
 const finishRide = () => {
-  stopRide();
+  isRiding.value = false;
+  isStopRiding.value = false;
+  clearWatch();
+  // TODO store locations
 };
 
 const onLongPressCallbackDirective = (e: PointerEvent) => {
-  finishRide();
+  setOpenFinishAlert(true);
 };
 
+const alertButtons = [
+  "Cancel",
+  {
+    text: "OK",
+    role: "confirm",
+    handler: () => {
+      finishRide();
+    },
+  },
+];
+const isOpenFinishAlert = ref(false);
+const setOpenFinishAlert = (isOpen: boolean) => {
+  isOpenFinishAlert.value = isOpen;
+};
 // onIonViewDidEnter(() => {
 //   requestLocationPermission();
 //   watchCurrentPosition((location) => {
@@ -246,6 +291,9 @@ ion-content {
         height: 80px;
         border-radius: 50%;
         transform: translate(36px, 8px);
+        span {
+          color: #fff;
+        }
       }
     }
 
