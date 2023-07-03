7430ffc4f3e071a3d911d8ddd993657adbc9f757
dongsp <dongsp@yxt.com>
2023-07-03 22:27:25 +0800

    fix some bugs

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,12 @@ import { useDisconnectEventBus } from "@/hooks/useDisconnectEventBus";
 import { useToast } from "@/hooks/useToast";
 import { useMessage } from "@/hooks/useMessage";
 import { useBluetoothLe } from "@/hooks/useBluetooth-le";
+import { useDashboardStore } from "@/store/useDashboardStore";
 
 const { stopSendMessage } = useMessage();
 const { scan } = useBluetoothLe();
 const store = useBleStore();
+const { resetDashboard } = useDashboardStore();
 const { on } = useDisconnectEventBus();
 const toast = useToast();
 on(async () => {
@@ -23,6 +25,7 @@ on(async () => {
   store.updateConnectedDevicePairedStatus(false);
   store.clearAvailableDevices();
   await stopSendMessage();
+  resetDashboard();
   await scan();
 });
 </script>
